6ba9ae577d13f33081c0b1d4aa63640636e94a82
Kris Kurzawa <kurzawa1@yahoo.com>
2023-06-13 22:07:19 -0400

    updated javascript

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-// Assignment Code
+// Assignment Code-original document
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input-original document next 3 lines
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var characterLengthInput = window.prompt("Enter length of your password. Password must be between 8-128 characters.");
+  var characterLengthInteger = parseInt(characterLengthInput);
+  while (isNaN(characterLengthInteger) || characterLengthInteger < 8 || characterLengthInteger > 128); {
+    characterLengthInput = window.prompt("Enter length of your password. Password must be between 8-128 characters.");
+    characterLengthInteger = parseInt(characterLengthInput);  
+  }
+  var includeLowercase = window.prompt("Would you like to include lower case letters in your password?  Reply yes or no.");
+  var includeLowercaseResponse = includeLowercase.includes("yes");
+  // while (includeLowercaseResponse != "yes" || includeLowercaseResponse != "no"); {
+  //   includeLowercase = window.prompt("Would you like to include lower case letters in your password?  Reply yes or no.");
+  //   includeLowercaseResponse = includeLowercase.includes("yes");
+  // }
+  // var includeLowercaseYes = true;
+  // var includeLowercaseRespnse = 
+  // var includeUppercase 
+  // get input from user w prompt
+  // validate that its yes or no
+  // for example for includeLowerCase if yes its true, else its false
+  console.log(characterLengthInteger);
+  console.log(includeLowercaseResponse);
 
+// original document
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button-original ducument
 generateBtn.addEventListener("click", writePassword);
+
